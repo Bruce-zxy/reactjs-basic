6ebb19c43ac634d161f38c176ff70d9a8b63d44c
Chris Wei <yunzhi.wei@live.com>
2017-05-08 17:00:11 +0800

    Dynamic mount component component as a variable

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      homeLink: "Home"
+      homeLink: "Home",
+      homeMounted: true
     }
   }
   onGreet() {
@@ -21,7 +22,25 @@ class App extends React.Component {
     });
   }
 
+  onChangeHomeMounted() {
+    this.setState({
+      homeMounted: !this.state.homeMounted
+    })
+  }
+
   render() {
+    let homeCmp = "";
+    if (this.state.homeMounted) {
+      homeCmp = (
+        <Home
+          name={"Max"}
+          initialAge={28}
+          greet={this.onGreet}
+          changeLink={this.onChangeLinkName.bind(this)}
+          initialLinkName={this.state.homeLink}
+          />
+        )
+    }
     return (
       <div className="container">
         <div className="row">
@@ -29,17 +48,19 @@ class App extends React.Component {
             <Header homeLink={this.state.homeLink} />
           </div>
         </div>
+
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home
-              name={"Max"}
-              initialAge={28}
-              greet={this.onGreet}
-              changeLink={this.onChangeLinkName.bind(this)}
-              initialLinkName={this.state.homeLink}
-              />
+            {homeCmp}
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-normal">(Un)Mount Home Component</button>
+          </div>
+        </div>
+
       </div>
     );
   }
