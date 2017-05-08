ac1f5a506c6d2b7646d8f370382651fbdd9c37cd
Chris Wei <yunzhi.wei@live.com>
2017-05-08 15:14:14 +0800

    Clean up a little bit

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,24 +2,10 @@ import React from "react";
 
 export class Home extends React.Component {
   render() {
-    var text = "Something!";
-
-    console.log(this.props);
-
     return(
       <div>
         <p>In a new Component</p>
-        <p>{text}</p>
         <p>Your name is {this.props.name}, your age is {this.props.age} </p>
-        <p>User Object => Name: {this.props.user.name}, Hobbies: {this.props.age} </p>
-        <div>
-          <h4>Hobbies:</h4>
-          <ul>
-            {this.props.user.hobbies.map((hobby, idx) => <li key={idx}>{idx + 1}. {hobby}</li>)}
-          </ul>
-        </div>
-        <hr/>
-        {this.props.children}
       </div>
     );
   }
@@ -27,7 +13,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: React.PropTypes.string,
-  age: React.PropTypes.number,
-  user: React.PropTypes.object,
-  children: React.PropTypes.element.isRequired
+  age: React.PropTypes.number
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,10 +6,6 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
   render() {
-    var user = {
-      name: "Chris",
-      hobbies: ["Sports", "Sleep"]
-    }
     return (
       <div className="container">
         <div className="row">
@@ -19,9 +15,7 @@ class App extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name={"Max"} age={28} user={user}>
-              <p>This is a props children</p>
-            </Home>
+            <Home name={"Max"} age={28} />
           </div>
         </div>
       </div>
