d12d0dff145eb1a5c442a17a9da0f1001ee1bcef
Chris Wei <yunzhi.wei@live.com>
2017-05-09 11:35:06 +0800

    pure immutable way: will never touch initialState throughout the program

--- src/app/index.js
+++ src/app/index.js
@@ -42,16 +42,17 @@ const reducer = (state = initialState, action) => {
     case "ADD":
       state = {
         ...state,
-        result: state.result + action.payload
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues]
       };
       state.lastValues.push(action.payload);
       break;
     case "SUBTRACT":
       state = {
         ...state,
-        result: state.result - action.payload
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
       };
-      state.lastValues.push(action.payload);
       break;
   }
   return state;
