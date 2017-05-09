40b6b98631df13d13dc9032f28b7e158d447595b
Chris Wei <yunzhi.wei@live.com>
2017-05-09 12:13:38 +0800

    Move previous code from index.js to App.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,37 +1,10 @@
-// import React from "react";
-// import {render} from "react-dom";
-//
-// import {User} from "./components/User";
-// import {Main} from "./components/Main";
-//
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: "Max"
-//     }
-//   }
-//
-//   changeUsername(newName) {
-//     this.setState({
-//       username: newName
-//     })
-//   }
-//
-//   render() {
-//     return (
-//       <div className="container">
-//         <Main changeUsername={this.changeUsername.bind(this)} />
-//         <User username={this.state.username} />
-//       </div>
-//     );
-//   }
-// }
-//
-// render(<App />, window.document.getElementById("app"));
+import React from "react";
+import {render} from "react-dom";
 
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import {createLogger} from "redux-logger";
+
+import {App} from "./components/App";
 
 const mathReducer = (state = {
     result: 1,
@@ -123,3 +96,5 @@ store.dispatch({
   type: "SET_NAME",
   payload: "Mike"
 });
+
+render(<App />, window.document.getElementById("app"));
