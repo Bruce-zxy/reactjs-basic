9340087ed66ac595574f45ebcf8deb792c0c7363
Chris Wei <yunzhi.wei@live.com>
2017-05-09 11:48:53 +0800

    multiple reducer

--- src/app/index.js
+++ src/app/index.js
@@ -30,14 +30,13 @@
 //
 // render(<App />, window.document.getElementById("app"));
 
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
-const initialState = {
-  result: 1,
-  lastValues: []
-};
-
-const reducer = (state = initialState, action) => {
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+  },
+  action) => {
   switch (action.type) {
     case "ADD":
       state = {
@@ -58,7 +57,29 @@ const reducer = (state = initialState, action) => {
   return state;
 };
 
-const store = createStore(reducer);
+const userReducer = (state = {
+    name: "Max",
+    age: 27
+  },
+  action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload
+      };
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        age: action.payload
+      };
+      break;
+  }
+  return state;
+};
+
+const store = createStore(combineReducers({mathReducer, userReducer}));
 
 store.subscribe(() => {
   console.log("Store updated!", store.getState());
@@ -77,4 +98,19 @@ store.dispatch({
 store.dispatch({
   type: "SUBTRACT",
   payload: 80
+});
+
+store.dispatch({
+  type: "SET_NAME",
+  payload: "Anna"
+});
+
+store.dispatch({
+  type: "SET_AGE",
+  payload: 80
+});
+
+store.dispatch({
+  type: "SET_NAME",
+  payload: "Mike"
 });
