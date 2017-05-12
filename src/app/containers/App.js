b1611dbab4fe7c193e1fa382f6a8656c00ae0752
Chris Wei <yunzhi.wei@live.com>
2017-05-12 11:56:33 +0800

    Prepare to display posts title

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,19 +1,43 @@
 import React from "react";
 import {connect} from "react-redux";
 
-import {User} from "../components/User";
 import {Main} from "../components/Main";
+import {User} from "../components/User";
+import {Posts} from "../components/Posts";
+
 import {setNameThunk, setNamePromise, setAge} from "../actions/userActions";
 
 class App extends React.Component {
   render() {
+    let posts = [
+      {
+        title: "Chris"
+      },
+      {
+        title: "xyz"
+      }
+    ];
+
     return (
       <div className="container">
+        <hr/>
+        <p>
+          <strong>Here is the area for Main</strong>
+        </p>
         <Main
           changeUsernameThunk={(name) => this.props.setNameThunk(name)}
           changeUsernamePromise={(name) => this.props.setNamePromise(name)}
         />
+        <hr/>
+        <p >
+          <strong>Here is the area for User</strong>
+        </p>
         <User username={this.props.user.name} />
+        <hr/>
+        <p>
+          <strong>Here is the area for Posts</strong>
+        </p>
+        <Posts posts={posts} />
       </div>
     );
   }
